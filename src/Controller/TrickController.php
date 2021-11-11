b1ca1f0aabf093dca3ebbcfd0fb836261a91e2ad
maintenantcjoey <legrand.jessica45@gmail.com>
2021-11-11 14:59:08 +0100

    Create trick entity and controller

---
+++ src/Controller/TrickController.php
@@ -0,0 +1,123 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Trick;
+use App\Form\TrickType;
+use App\Repository\TrickRepository;
+use App\Service\FileManager;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/trick")
+ */
+class TrickController extends AbstractController
+{
+    /**
+     * @var \App\Service\FileManager
+     */
+    private $fileManager;
+
+    public function __construct(FileManager $fileManager)
+    {
+        $this->fileManager = $fileManager;
+    }
+
+    /**
+     * @Route(name="trick_index", methods={"GET"})
+     */
+    public function index(TrickRepository $trickRepository): Response
+    {
+        $tricks = $trickRepository->findAll();
+        return $this->render('trick/index.html.twig', [
+            'tricks' => $tricks,
+        ]);
+    }
+
+    /**
+     * @Route("/new", name="trick_new", methods={"GET","POST"})
+     */
+    public function new(Request $request): Response
+    {
+        $trick = new Trick();
+        $form = $this->createForm(TrickType::class, $trick);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $this->handlePictures($form->get('pictures'));
+            $entityManager->persist($trick);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('home', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('trick/new.html.twig', [
+            'trick' => $trick,
+            'form' => $form,
+        ]);
+    }
+
+    /**
+     * @Route("/{trick_group_slug}/{slug}", name="trick_show", methods={"GET"}, priority=-1)
+     */
+    public function show(Trick $trick): Response
+    {
+        return $this->render('trick/show.html.twig', [
+            'trick' => $trick,
+        ]);
+    }
+
+    /**
+     * @Route("/{id<\d+>}/edit", name="trick_edit", methods={"GET","POST"})
+     */
+    public function edit(Request $request, Trick $trick): Response
+    {
+        $form = $this->createForm(TrickType::class, $trick);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->handlePictures($form->get('pictures'), true);
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('trick_show', [
+                'slug' => $trick->getSlug(),
+                'trick_group_slug' => $trick->getTrickGroup()->getSlug()
+            ], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('trick/edit.html.twig', [
+            'trick' => $trick,
+            'form' => $form,
+        ]);
+    }
+
+    /**
+     * @Route("/{id<\d+>}", name="trick_delete", methods={"POST"})
+     */
+    public function delete(Request $request, Trick $trick): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$trick->getId(), $request->request->get('_token'))) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($trick);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('home', [], Response::HTTP_SEE_OTHER);
+    }
+
+    public function handlePictures($pictures, $update = false)
+    {
+        foreach ($pictures as $picture) {
+            try {
+                $this->fileManager->upload($picture, $update);
+            } catch (FileException $e) {
+                continue;
+            }
+        }
+    }
+}
---
+++ src/Entity/Picture.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\MediaRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ */
+class Picture
+{
+
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $path;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Trick::class, inversedBy="pictures")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $trick;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getPath(): ?string
+    {
+        return $this->path;
+    }
+
+    public function setPath(string $path): self
+    {
+        $this->path = $path;
+
+        return $this;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getTrick(): ?Trick
+    {
+        return $this->trick;
+    }
+
+    public function setTrick(?Trick $trick): self
+    {
+        $this->trick = $trick;
+
+        return $this;
+    }
+}
